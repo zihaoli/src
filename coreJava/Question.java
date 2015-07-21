f2d21ba059493f826f1ee041531b085a8dd42cf9
zihaoli <vip100546@163.com>
2015-07-21 17:17:18 +0800

    5

---
+++ coreJava/Question.java
@@ -0,0 +1,82 @@
+package coreJava;
+
+
+
+public class Question {
+	private int id;
+	private String title;
+
+	private String[] options;
+
+//	public Question() {
+//	}
+
+	public Question(int id, String title, String[] options) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.options = options;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String[] getOptions() {
+		return options;
+	}
+
+	public void setOptions(String[] options) {
+		this.options = options;
+	}
+
+	public boolean check(String anwser) {
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer s = new StringBuffer();
+		s.append(id).append(".").append(title).append("\n");
+		for(String v:options){
+			s.append(v).append("\n");
+		}
+		return s.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+
+}
