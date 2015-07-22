4eb344b96138b5384b4dbc1f9d311b7e94a2855e
zihaoli <vip100546@163.com>
2015-07-22 17:47:50 +0800

    f

--- coreJava/QuesDemo1.java
+++ coreJava/QuesDemo1.java
@@ -3,9 +3,9 @@ package coreJava;
 public class QuesDemo1 {
 
 	public static void main(String[] args) {
-		Question q1 = new SingleQuestion()(1, "爱情电影是：", new String[] { "A.失落", "B.风雨","C.权利" }null);
+		Question q1 = new SingleQuestion(1, "爱情电影是：", new String[] { "A.失落", "B.风雨","C.权利" }, "A");
 		System.out.print(q1);
-		System.out.println(q1.check("a"));
+		System.out.println(q1.check("A"));
 		System.out.println();
 		SingleQuestion q2 = new SingleQuestion(1, "下面是开发语言的是", new String[] {
 				"A.java", "B.tt" }, "A");
