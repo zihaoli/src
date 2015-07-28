533cfd819c31bb5aa98be8995de8b46adcfcbd18
zihaoli <vip100546@163.com>
2015-07-28 16:47:02 +0800

    发

---
+++ movie/Customer.java
@@ -0,0 +1,72 @@
+package movie;
+
+public class Customer {
+	private String name;
+	private Rental[] rs = new Rental[5];
+	private int index = 0;
+
+	public Customer() {
+	}
+
+	public Customer(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void addRental(Rental rental) {
+		rs[index++] = rental;
+	}
+
+	// 打印金额，积分情况
+	public void printMessage() {
+		double totalprice = 0;
+		double result = 0;
+		int fre = 0;
+		for (int i = 0; i < index; i++) { // 小于index，因为不是rs全部放满的
+			
+			//Rental r = rs[i];
+			switch (rs[i].getMovie().getMovieCode()) {
+			case Movie.NEW_MOVIE:
+				
+				totalprice += rs[i].getDays() * 3;
+				break;
+			case Movie.CHILDEN_MOVIE:
+				
+				if (rs[i].getDays() <= 3) {
+					totalprice += 1.5;
+				} else {
+					totalprice += (rs[i].getDays() - 3) * 1.5 + 1.5;
+				}
+				break;
+			case Movie.R_MOVIE:
+				
+				if (rs[i].getDays() <= 2) {
+					totalprice += 2;
+				} else {
+					totalprice += (rs[i].getDays() - 2) *2+ 1.5;
+				}
+				break;
+			}
+			
+			
+			//totalprice += result;
+			fre++;
+		
+			if (rs[i].getMovie().getMovieCode() == Movie.NEW_MOVIE&&rs[i].getDays()>1)
+				fre++;
+			System.out.println(totalprice);
+		}
+		System.out.println("你所消费的金额是" + totalprice + "元");
+		System.out.println("你的积分是" + fre);
+
+	}
+
+}
---
+++ movie/Movietest.java
@@ -0,0 +1,25 @@
+package movie;
+
+public class Movietest {
+	public static void main(String[] args) {
+		//创建影片
+		Movie m1 = new Movie("喜洋洋");
+		m1.setMovieCode(Movie.CHILDEN_MOVIE);
+		Movie m2 = new Movie("复仇者2");
+		m2.setMovieCode(Movie.NEW_MOVIE);
+		Movie m3 = new Movie("普通电影");
+		m3.setMovieCode(Movie.R_MOVIE);
+		
+		Customer c1 = new Customer("张三");
+		Rental r1 = new Rental(m1, 8);
+		Rental r2 = new Rental(m2, 9);
+		Rental r3 = new Rental(m3,9);
+		
+		c1.addRental(r1);
+		c1.addRental(r2);
+		c1.addRental(r3);
+		c1.printMessage();
+		
+	}
+
+}
