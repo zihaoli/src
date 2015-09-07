a9f6c3ab5b8c5dbaa83788da31c9c802a379017b
zihaoli <vip100546@163.com>
2015-09-07 16:22:54 +0800

    d

---
+++ corelession1/MathDemo.java
@@ -0,0 +1,12 @@
+package corelession1;
+
+public class MathDemo {
+public static void main(String[] args) {
+	System.out.println(Math.PI);
+	System.out.println(Math.abs(-10));
+	System.out.println(Math.floor(10.3));
+	System.out.println(Math.floor(10.5));
+	System.out.println(Math.round(10.2));
+	System.out.println(Math.round(10.5));
+}
+}
---
+++ corelession1/RegexDemo5.java
@@ -0,0 +1,13 @@
+package corelession1;
+
+public class RegexDemo5 {
+	public static void main(String[] args) {
+		//常用的正则变大时都可以从网络中搜索到，不一定都要自己去写
+		//定义一个邮箱
+		
+		String regex = "^\\w+@[a-zA-Z0-9]+(\\.[a-zA-Z]+)+$)";
+		
+		
+	}
+
+}
